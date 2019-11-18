c8c3408ee3448dfe704c799012e50999b6902c93
TakahiroSato <futurenova52@gmail.com>
2019-11-18 16:48:44 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 extern crate ws;
 
 use std::cell::{Cell, RefCell};
+use std::f32;
 use std::rc::Rc;
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Instant;
 
 use ws::{listen, CloseCode, Error, Handler, Handshake, Message, Result, Sender};
 
@@ -9,30 +13,49 @@ pub mod json_parse;
 pub mod mgr;
 
 struct Server {
-  out: Sender,
+  out: Arc<Mutex<Sender>>,
   count: Rc<Cell<u32>>,
-  users: RefCell<mgr::Users>,
+  users: Arc<Mutex<mgr::Users>>,
 }
 
 impl Handler for Server {
   fn on_open(&mut self, _: Handshake) -> Result<()> {
-    let id = format!("id-{}", self.out.connection_id().to_string());
-    self.users.borrow_mut().add_user(&id);
-    self.out.send(format!("open: Your ID is {}", &id)).unwrap();
+    let out = self.out.lock().unwrap();
+    let mut users = self.users.lock().unwrap();
+    let id = format!("id-{}", out.connection_id().to_string());
+    users.add_user(&id, 100, 100, 0);
     Ok(self.count.set(self.count.get() + 1))
   }
 
   fn on_message(&mut self, msg: Message) -> Result<()> {
+    let out = self.out.lock().unwrap();
     println!("The number of live connection is {}", self.count.get());
     let json = json_parse::parse(msg.to_string()).unwrap();
-    for user in self.users.borrow_mut().list.iter_mut() {
-      if user.id == format!("id-{}", self.out.connection_id().to_string()) {
-        let x: i32 = json["x"].to_string().parse().unwrap();
-        let y: i32 = json["y"].to_string().parse().unwrap();
-        user.set_data(x, y);
-        self.out.broadcast(Message::from(user.id.to_string())).unwrap();
-        let _msg = format!("x={}, y={}", x.to_string(), y.to_string());
-        self.out.broadcast(Message::from(_msg)).unwrap();
+    let mut users = self.users.lock().unwrap();
+    for user in users.list.iter_mut() {
+      if user.id == format!("id-{}", out.connection_id().to_string()) {
+        let left: u8 = json["left"].to_string().parse().unwrap();
+        let up: u8 = json["up"].to_string().parse().unwrap();
+        let right: u8 = json["right"].to_string().parse().unwrap();
+        let down: u8 = json["down"].to_string().parse().unwrap();
+        let add_degree = if left == 1 {
+          -5
+        } else if right == 1 {
+          5
+        } else {
+          0
+        };
+        let rad: f32 =
+          (f32::consts::PI * (user.degree + add_degree) as f32 / 180.) - f32::consts::PI / 2.;
+        let (mut addx, mut addy) = (0, 0);
+        if up == 1 {
+          addx = (rad.cos() * 5.) as i32;
+          addy = (rad.sin() * 5.) as i32;
+        } else if down == 1 {
+          addx = -(rad.cos() * 5.) as i32;
+          addy = -(rad.sin() * 5.) as i32;
+        }
+        user.add(addx, addy, add_degree);
       }
     }
     Ok(())
@@ -47,6 +70,12 @@ impl Handler for Server {
       }
       _ => println!("The client encountered an error: {}", reason),
     }
+    let out = self.out.lock().unwrap();
+    self
+      .users
+      .lock()
+      .unwrap()
+      .remove_user(&format!("id-{}", out.connection_id().to_string()));
     self.count.set(self.count.get() - 1)
   }
 
@@ -57,10 +86,36 @@ impl Handler for Server {
 
 fn main() {
   let count = Rc::new(Cell::new(0));
-  listen("127.0.0.1:3012", |out| Server {
-    out: out,
-    count: count.clone(),
-    users: RefCell::new(mgr::Users { list: vec![] })
+  let users = Arc::new(Mutex::new(mgr::Users { list: vec![] }));
+  let mut flag = false;
+  listen("127.0.0.1:3012", |out| {
+    let o = Arc::new(Mutex::new(out));
+    if flag == false {
+      let _o = o.clone();
+      let _users = users.clone();
+      thread::spawn(move || {
+        println!("spawn");
+        let mut time = Instant::now();
+        loop {
+          game_main(&mut time, &_o.lock().unwrap(), &_users.lock().unwrap());
+        }
+      });
+      flag = true;
+    }
+    let ret = Server {
+      out: o.clone(),
+      count: count.clone(),
+      users: users.clone(),
+    };
+    ret
   })
   .unwrap()
+}
+
+const FPS: u32 = 30;
+fn game_main(time: &mut Instant, out: &Sender, users: &mgr::Users) {
+  if time.elapsed().as_millis() >= (1000. / (FPS as f32)) as u128 {
+    out.broadcast(Message::from(users.get_json())).unwrap();
+    *time = Instant::now();
+  }
 }
--- src/mgr.rs
+++ src/mgr.rs
@@ -1,30 +1,59 @@
 use crate::json_parse;
+use serde::{Deserialize, Serialize};
+use serde_json::to_string;
 
-//use std::cell::{Cell, RefCell};
-use serde_json::{Value, json};
-use uuid::Uuid;
-
+#[derive(Serialize, Deserialize, Clone)]
 pub struct User {
   pub id: String,
-  pub data: Value
+  pub x: i32,
+  pub y: i32,
+  pub degree: i32,
 }
 
 impl User {
-  pub fn set_data(&mut self, x: i32, y: i32) {
-    self.data["x"] = json!(x);
-    self.data["y"] = json!(y);
+  pub fn set_data(&mut self, x: i32, y: i32, degree: i32) {
+    self.x = x;
+    self.y = y;
+    self.degree = degree;
+  }
+
+  pub fn add(&mut self, x: i32, y: i32, degree: i32) {
+    self.x += x;
+    self.y += y;
+    self.degree += degree;
+  }
+
+  pub fn get_json(&self) -> String {
+    serde_json::to_string(&self).unwrap()
   }
 }
 
+#[derive(Serialize, Deserialize)]
 pub struct Users {
-  pub list: Vec<User>
+  pub list: Vec<User>,
 }
 
 impl Users {
-  pub fn add_user(&mut self, id: &String) {
+  pub fn add_user(&mut self, id: &String, x: i32, y: i32, degree: i32) {
     self.list.push(User {
       id: id.to_string(),
-      data: json_parse::parse(r#"{"x":0, "y":0}"#.to_string()).unwrap()
+      x: x,
+      y: y,
+      degree: degree,
     });
+  }
+
+  pub fn remove_user(&mut self, id: &String) {
+    self.list = self
+      .list
+      .iter()
+      .filter(|&user| user.id != id.to_string())
+      .cloned()
+      .collect();
+  }
+
+  pub fn get_json(&self) -> String {
+    let ret = serde_json::to_string(&self).unwrap();
+    ret
   }
 }
