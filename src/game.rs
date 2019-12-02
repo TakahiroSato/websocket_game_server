462ba226c3b526beb4697dd83dce233e4f37902e
TakahiroSato <futurenova52@gmail.com>
2019-12-02 16:14:21 +0900

    微変更

--- src/game.rs
+++ src/game.rs
@@ -1,8 +1,6 @@
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::f32;
-use std::sync::Mutex;
-use std::time::Instant;
 use ws::{Message, Sender};
 
 use crate::bullet::Bullet;
@@ -10,130 +8,130 @@ use crate::user::User;
 
 #[derive(Serialize, Deserialize)]
 pub struct Game {
-  pub users: Vec<User>,
-  pub bullets: Vec<Bullet>,
+    pub users: Vec<User>,
+    pub bullets: Vec<Bullet>,
 }
 
 const WINDOW_WIDTH: i32 = 1080;
 const WINDOW_HEIGHT: i32 = 720;
 const SPEED: i32 = 10;
-const FPS: u32 = 60;
+pub const FPS: f32 = 60.;
 impl Game {
-  pub fn main(time: &mut Instant, out: &Sender, game: &Mutex<Game>) {
-    if time.elapsed().as_millis() >= (1000. / (FPS as f32)) as u128 {
-      let mut _game = game.lock().unwrap();
-      _game.bullets = _game
-        .bullets
-        .iter()
-        .filter(|b| {
-          b.live && b.x >= -10 && b.x <= WINDOW_WIDTH + 10 && b.y >= -10 && b.y <= WINDOW_HEIGHT + 10
-        })
-        .cloned()
-        .collect();
-      for bullet in _game.bullets.iter_mut() {
-        bullet.set_position();
-      }
-      _game.collision();
-      out.broadcast(Message::from(_game.get_json())).unwrap();
-      *time = Instant::now();
-    }
-  }
-
-  fn collision(&mut self) {
-    let mut bullets = self.bullets.clone();
-    for user in self.users.iter_mut() {
-      for b in bullets.iter_mut() {
-        let dx = user.x - b.x;
-        let dy = user.y - b.y;
-        if dx * dx + dy * dy <= 650 {
-          user.life -= 1;
-          b.live = false;
+    pub fn main(&mut self, out: &Sender) {
+        self.bullets = self
+            .bullets
+            .iter()
+            .filter(|b| {
+                b.live
+                    && b.x >= -10
+                    && b.x <= WINDOW_WIDTH + 10
+                    && b.y >= -10
+                    && b.y <= WINDOW_HEIGHT + 10
+            })
+            .cloned()
+            .collect();
+        for bullet in self.bullets.iter_mut() {
+            bullet.set_position();
         }
-      }
+        self.collision();
+        out.broadcast(Message::from(self.get_json())).unwrap();
     }
-    self.bullets = bullets;
-  }
 
-  pub fn add_user(&mut self, id: &String, x: i32, y: i32, degree: i32) {
-    self.users.push(User {
-      id: id.to_string(),
-      x: x,
-      y: y,
-      degree: degree,
-      life: 100,
-    });
-  }
-
-  pub fn remove_user(&mut self, id: &String) {
-    self.users = self
-      .users
-      .iter()
-      .filter(|&user| user.id != id.to_string())
-      .cloned()
-      .collect();
-  }
-
-  fn add_bullet(&mut self, x: i32, y: i32, mx: i32, my: i32) {
-    self.bullets.push(Bullet {
-      x: x,
-      y: y,
-      mx: mx,
-      my: my,
-      live: true,
-    });
-  }
-
-  pub fn set_position(&mut self, json: Value, id: &String) {
-    let mut users = self.users.clone();
-    for user in users.iter_mut() {
-      if user.id == format!("id-{}", id) {
-        let f = |j: &Value, k| {
-          let ret: u8 = match j[k].to_string().parse() {
-            Ok(v) => v,
-            Err(_) => 0,
-          };
-          ret
-        };
-        let left: u8 = f(&json, "left");
-        let up: u8 = f(&json, "up");
-        let right: u8 = f(&json, "right");
-        let down: u8 = f(&json, "down");
-        let add_degree = if left == 1 {
-          -SPEED
-        } else if right == 1 {
-          SPEED
-        } else {
-          0
-        };
-        let rad: f32 =
-          (f32::consts::PI * (user.degree + add_degree) as f32 / 180.) - f32::consts::PI / 2.;
-        let (mut addx, mut addy) = (0, 0);
-        if up == 1 {
-          addx = (rad.cos() * SPEED as f32) as i32;
-          addy = (rad.sin() * SPEED as f32) as i32;
-        } else if down == 1 {
-          addx = -(rad.cos() * SPEED as f32) as i32;
-          addy = -(rad.sin() * SPEED as f32) as i32;
+    fn collision(&mut self) {
+        let mut bullets = self.bullets.clone();
+        for user in self.users.iter_mut() {
+            for b in bullets.iter_mut() {
+                let dx = user.x - b.x;
+                let dy = user.y - b.y;
+                if dx * dx + dy * dy <= 650 {
+                    user.life -= 1;
+                    b.live = false;
+                }
+            }
         }
-        user.add(addx, addy, add_degree);
-
-        if f(&json, "shot") == 1 {
-          let bx = user.x + (rad.cos() * 30.) as i32;
-          let by = user.y + (rad.sin() * 30.) as i32;
-          self.add_bullet(
-            bx,
-            by,
-            (rad.cos() * SPEED as f32) as i32,
-            (rad.sin() * SPEED as f32) as i32,
-          );
-        }
-      }
+        self.bullets = bullets;
     }
-    self.users = users;
-  }
 
-  pub fn get_json(&self) -> String {
-    let ret = serde_json::to_string(&self).unwrap();
-    ret
-  }
+    pub fn add_user(&mut self, id: &String, x: i32, y: i32, degree: i32) {
+        self.users.push(User {
+            id: id.to_string(),
+            x: x,
+            y: y,
+            degree: degree,
+            life: 100,
+        });
+    }
+
+    pub fn remove_user(&mut self, id: &String) {
+        self.users = self
+            .users
+            .iter()
+            .filter(|&user| user.id != id.to_string())
+            .cloned()
+            .collect();
+    }
+
+    fn add_bullet(&mut self, x: i32, y: i32, mx: i32, my: i32) {
+        self.bullets.push(Bullet {
+            x: x,
+            y: y,
+            mx: mx,
+            my: my,
+            live: true,
+        });
+    }
+
+    pub fn set_position(&mut self, json: Value, id: &String) {
+        let mut users = self.users.clone();
+        for user in users.iter_mut() {
+            if user.id == format!("id-{}", id) {
+                let f = |j: &Value, k| {
+                    let ret: u8 = match j[k].to_string().parse() {
+                        Ok(v) => v,
+                        Err(_) => 0,
+                    };
+                    ret
+                };
+                let left: u8 = f(&json, "left");
+                let up: u8 = f(&json, "up");
+                let right: u8 = f(&json, "right");
+                let down: u8 = f(&json, "down");
+                let add_degree = if left == 1 {
+                    -SPEED
+                } else if right == 1 {
+                    SPEED
+                } else {
+                    0
+                };
+                let rad: f32 = (f32::consts::PI * (user.degree + add_degree) as f32 / 180.)
+                    - f32::consts::PI / 2.;
+                let (mut addx, mut addy) = (0, 0);
+                if up == 1 {
+                    addx = (rad.cos() * SPEED as f32) as i32;
+                    addy = (rad.sin() * SPEED as f32) as i32;
+                } else if down == 1 {
+                    addx = -(rad.cos() * SPEED as f32) as i32;
+                    addy = -(rad.sin() * SPEED as f32) as i32;
+                }
+                user.add(addx, addy, add_degree);
+
+                if f(&json, "shot") == 1 {
+                    let bx = user.x + (rad.cos() * 30.) as i32;
+                    let by = user.y + (rad.sin() * 30.) as i32;
+                    self.add_bullet(
+                        bx,
+                        by,
+                        (rad.cos() * SPEED as f32) as i32,
+                        (rad.sin() * SPEED as f32) as i32,
+                    );
+                }
+            }
+        }
+        self.users = users;
+    }
+
+    pub fn get_json(&self) -> String {
+        let ret = serde_json::to_string(&self).unwrap();
+        ret
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,77 +7,85 @@ use std::thread;
 use std::time::Instant;
 
 use my_websocket::{game, json_parse};
-use ws::{listen, CloseCode, Error, Handler, Handshake, Message, Result, Sender};
+use ws::{CloseCode, Error, Handler, Handshake, Message, Result, Sender, WebSocket};
 
 struct Server {
-  out: Arc<Mutex<Sender>>,
-  count: Rc<Cell<u32>>,
-  game: Arc<Mutex<game::Game>>,
+    out: Arc<Mutex<Sender>>,
+    count: Rc<Cell<u32>>,
+    game: Arc<Mutex<game::Game>>,
 }
 
 impl Handler for Server {
-  fn on_open(&mut self, _: Handshake) -> Result<()> {
-    let out = self.out.lock().unwrap();
-    let mut g = self.game.lock().unwrap();
-    let id = format!("id-{}", out.connection_id().to_string());
-    g.add_user(&id, 100, 100, 135);
-    self.count.set(self.count.get() + 1);
-    Ok(())
-  }
-
-  fn on_message(&mut self, msg: Message) -> Result<()> {
-    let out = self.out.lock().unwrap();
-    let json = json_parse::parse(msg.to_string()).unwrap();
-    let mut g = self.game.lock().unwrap();
-    g.set_position(json, &out.connection_id().to_string());
-    Ok(())
-  }
-
-  fn on_close(&mut self, code: CloseCode, reason: &str) {
-    match code {
-      CloseCode::Normal => println!("The client is done with the connection."),
-      CloseCode::Away => println!("The client is leaving the site."),
-      CloseCode::Abnormal => {
-        println!("Closing handshake failed! Unable to obtain closing status from client.")
-      }
-      _ => println!("The client encountered an error: {}", reason),
+    fn on_open(&mut self, _: Handshake) -> Result<()> {
+        let out = self.out.lock().unwrap();
+        let mut g = self.game.lock().unwrap();
+        let id = format!("id-{}", out.connection_id().to_string());
+        g.add_user(&id, 100, 100, 135);
+        self.count.set(self.count.get() + 1);
+        println!("count : {}", self.count.get());
+        Ok(())
     }
-    let out = self.out.lock().unwrap();
-    self
-      .game
-      .lock()
-      .unwrap()
-      .remove_user(&format!("id-{}", out.connection_id().to_string()));
-    self.count.set(self.count.get() - 1)
-  }
 
-  fn on_error(&mut self, err: Error) {
-    println!("The server encountered an error: {:?}", err);
-  }
+    fn on_message(&mut self, msg: Message) -> Result<()> {
+        let out = self.out.lock().unwrap();
+        let json = json_parse::parse(msg.to_string()).unwrap();
+        let mut g = self.game.lock().unwrap();
+        g.set_position(json, &out.connection_id().to_string());
+        Ok(())
+    }
+
+    fn on_close(&mut self, code: CloseCode, reason: &str) {
+        match code {
+            CloseCode::Normal => println!("The client is done with the connection."),
+            CloseCode::Away => println!("The client is leaving the site."),
+            CloseCode::Abnormal => {
+                println!("Closing handshake failed! Unable to obtain closing status from client.")
+            }
+            _ => println!("The client encountered an error: {}", reason),
+        }
+        let out = self.out.lock().unwrap();
+        self.game
+            .lock()
+            .unwrap()
+            .remove_user(&format!("id-{}", out.connection_id().to_string()));
+        self.count.set(self.count.get() - 1);
+        println!("count : {}", self.count.get());
+    }
+
+    fn on_error(&mut self, err: Error) {
+        println!("The server encountered an error: {:?}", err);
+    }
 }
 
 fn main() {
-  let count = Rc::new(Cell::new(0));
-  let g = Arc::new(Mutex::new(game::Game { users: vec![], bullets: vec![] }));
-  let mut is_inited = false;
-  listen("127.0.0.1:3012", |out| {
-    let o = Arc::new(Mutex::new(out));
-    if !is_inited {
-      let _o = o.clone();
-      let _g = g.clone();
-      thread::spawn(move || {
-        let mut time = Instant::now();
-        loop {
-          game::Game::main(&mut time, &_o.lock().unwrap(), &_g);
+    let count = Rc::new(Cell::new(0));
+    let g = Arc::new(Mutex::new(game::Game {
+        users: vec![],
+        bullets: vec![],
+    }));
+    let mut is_inited = false;
+    let ws = WebSocket::new(move |out: Sender| {
+        let out = Arc::new(Mutex::new(out.clone()));
+        if !is_inited {
+            let out = out.clone();
+            let g = g.clone();
+            thread::spawn(move || {
+                let mut time = Instant::now();
+                loop {
+                    if time.elapsed().as_millis() >= (1000. / game::FPS) as u128 {
+                        g.lock().unwrap().main(&out.lock().unwrap());
+                        time = Instant::now();
+                    }
+                }
+            });
+            is_inited = true;
         }
-      });
-      is_inited = true;
-    }
-    Server {
-      out: o.clone(),
-      count: count.clone(),
-      game: g.clone(),
-    }
-  })
-  .unwrap();
+        Server {
+            out: out,
+            count: count.clone(),
+            game: g.clone(),
+        }
+    })
+    .unwrap();
+    ws.listen("0.0.0.0:3012").unwrap();
 }
